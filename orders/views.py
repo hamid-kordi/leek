dff1e7dd2ef3a7fa472dd0c12e7d2d70bf0fccc5
Hmidreza <hmidreza@Hmidrezas-MacBook-Pro.local>
2024-07-16 18:02:05 +0330

    fix:

--- orders/views.py
+++ orders/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
-
+from rest_framework.throttling import UserRateThrottle, AnonRateThrottle
 from .models import OrderItem, Orders, Report
 from rest_framework import status
 from django.shortcuts import get_object_or_404
